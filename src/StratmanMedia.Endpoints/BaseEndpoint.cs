2998b40b203bdd493c0de58bebe1c72a8b6facc5
Jason Stratman <jason@stratmanmedia.com>
2021-08-18 20:58:37 -0500

    Added endpoints library

---
+++ src/StratmanMedia.Endpoints/BaseEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StratmanMedia.Endpoints
+{
+    [ApiController]
+    public static class BaseEndpoint
+    {
+        public static class WithRequest<TRequest>
+        {
+            public abstract class WithResponse<TResponse> : ControllerBase
+            {
+                public abstract Task<ActionResult<TResponse>> HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithoutResponse : ControllerBase
+            {
+                public abstract Task<ActionResult> HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
+
+        public static class WithoutRequest
+        {
+            public abstract class WithResponse<TResponse> : ControllerBase
+            {
+                public abstract Task<ActionResult<TResponse>> HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithoutResponse : ControllerBase
+            {
+                public abstract Task<ActionResult> HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
+    }
+}
